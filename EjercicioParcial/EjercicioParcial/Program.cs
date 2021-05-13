08b4b86f1b57687dac42f62c82e6a0b172ac03e4
ha0372 <ha0372032020@unab.edu.sv>
2021-05-13 15:24:21 -0600

    ejer

---
+++ EjercicioParcial/EjercicioParcial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int dato1 = 16;
+                String dato2 = "25g";
+                int resultado = dato1 + Convert.ToInt32(dato2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+            }
+
+
+
+            Console.ReadKey();
+            //Console.WriteLine(resultado);
+        }
+    }
+}
